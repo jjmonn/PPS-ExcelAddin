82b0f683d8a25d45b60083a852769e3b7cd75e04
jjmonn <monnereau.julien@gmail.com>
2015-02-07 16:24:39 +0100

    Mappings/pps_colorsMapping.vb added

---
+++ PPS/Models/Mappings/pps_colorsMapping.vb
@@ -0,0 +1,38 @@
+﻿'
+'
+'
+'
+' Author: Julien Monnereau
+' Last modified: 05/02/2015
+
+
+Imports System.Collections.Generic
+Imports System.Collections
+
+
+Friend Class pps_colorsMapping
+
+
+    Protected Friend Shared Function GetColorsList() As List(Of Hashtable)
+
+        Dim tmpList As New List(Of Hashtable)
+        Dim srv As New ModelServer
+        srv.OpenRst(CONFIG_DATABASE + "." + PPS_COLORS_TABLE, ModelServer.FWD_CURSOR)
+        If srv.rst.EOF = False And srv.rst.BOF = False Then
+            srv.rst.MoveFirst()
+            Do While srv.rst.EOF = False
+                Dim ht As New Hashtable
+                ht.Add(PPS_COLORS_RED_VAR, srv.rst.Fields(PPS_COLORS_RED_VAR).Value)
+                ht.Add(PPS_COLORS_GREEN_VAR, srv.rst.Fields(PPS_COLORS_GREEN_VAR).Value)
+                ht.Add(PPS_COLORS_BLUE_VAR, srv.rst.Fields(PPS_COLORS_BLUE_VAR).Value)
+                tmpList.Add(ht)
+                srv.rst.MoveNext()
+            Loop
+        End If
+        srv.rst.Close()
+        Return tmpList
+
+    End Function
+
+
+End Class
