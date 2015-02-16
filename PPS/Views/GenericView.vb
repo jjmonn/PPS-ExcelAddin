3c2ba8e67946c84199b7dfe58cf872bfd5fa3a2b
jjmonn <monnereau.julien@gmail.com>
2015-02-16 06:57:13 +0100

    Files Creation

---
+++ PPS/Views/GenericView.vb
@@ -0,0 +1,14 @@
+﻿
+Friend Class GenericView
+
+    Protected Friend Sub New(ByVal title As String)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.Text = title
+
+    End Sub
+
+End Class
