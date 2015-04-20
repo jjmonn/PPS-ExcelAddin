cb6a69ae702c9d282dcb7d3bf4bf558d0e2195f6
jjmonn <monnereau.julien@gmail.com>
2015-04-20 08:27:57 +0200

    Add CUI2DisplayControl

---
+++ CUI2DisplayControl/CUI2DisplayControl/DisplayControl.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DisplayControl
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.rows_display_tv = New System.Windows.Forms.TreeView()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.rows_display_tv, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60.56604!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.43396!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(214, 530)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'rows_display_tv
+        '
+        Me.rows_display_tv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rows_display_tv.Location = New System.Drawing.Point(3, 324)
+        Me.rows_display_tv.Name = "rows_display_tv"
+        Me.rows_display_tv.Size = New System.Drawing.Size(208, 203)
+        Me.rows_display_tv.TabIndex = 1
+        '
+        'DisplayControl
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "DisplayControl"
+        Me.Size = New System.Drawing.Size(214, 530)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Public WithEvents rows_display_tv As System.Windows.Forms.TreeView
+
+End Class
---
+++ CUI2DisplayControl/CUI2DisplayControl/DisplayControl.vb
@@ -0,0 +1,97 @@
+﻿' DisplayControl.vb
+'
+' User interface for configuring controllingUI2 display
+' Allows user ot drag and drop nodes from analysis axis treeview to rows_display_tv
+'
+'
+'
+'
+' Author: Julien Monnereau
+' Last modified: 14/04/2015
+
+
+
+Public Class DisplayControl
+
+
+#Region "Instance Variables"
+
+    ' Objects
+    Private analysis_axis_tv As TreeView
+
+
+
+#End Region
+
+
+#Region "Interface"
+
+    Public Sub New(ByRef analysis_axis_tv As TreeView)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.analysis_axis_tv = analysis_axis_tv
+        TableLayoutPanel1.Controls.Add(analysis_axis_tv, 0, 0)
+        analysis_axis_tv.Dock = DockStyle.Fill
+        rows_display_tv.AllowDrop = True
+
+        ' By default add the fisrt node
+        rows_display_tv.Nodes.Add(analysis_axis_tv.Nodes(0).Name, analysis_axis_tv.Nodes(0).Name)
+
+        AddHandler analysis_axis_tv.ItemDrag, AddressOf analysisAxisTV_ItemDrag
+        AddHandler analysis_axis_tv.DragEnter, AddressOf analysisAxisTV_DragEnter
+
+    End Sub
+
+#End Region
+
+
+#Region "Drag and Drop Procedure"
+
+    Private Sub analysisAxisTV_ItemDrag(sender As Object, e As ItemDragEventArgs)
+
+        DoDragDrop(e.Item, Windows.Forms.DragDropEffects.Move)
+
+    End Sub
+
+    Private Sub analysisAxisTV_DragEnter(sender As Object, e As Windows.Forms.DragEventArgs)
+
+        If e.Data.GetDataPresent("System.Windows.Forms.TreeNode", True) Then
+            e.Effect = DragDropEffects.Move
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub rowDisplayTV_DragOver(sender As Object, e As Windows.Forms.DragEventArgs) Handles rows_display_tv.DragOver
+
+        If e.Data.GetDataPresent("System.Windows.Forms.TreeNode", True) = False Then Exit Sub
+        e.Effect = DragDropEffects.Move
+
+    End Sub
+
+    Private Sub rowDisplayTV_DragDrop(sender As Object, e As Windows.Forms.DragEventArgs) Handles rows_display_tv.DragDrop
+
+        'Check that there is a TreeNode being dragged
+        If e.Data.GetDataPresent("System.Windows.Forms.TreeNode", True) = False Then Exit Sub
+
+        Dim selectedTreeview As TreeView = CType(sender, Windows.Forms.TreeView)
+
+        'Get the TreeNode being dragged
+        Dim dropNode As TreeNode = CType(e.Data.GetData("System.Windows.Forms.TreeNode"), TreeNode)
+
+        'The target node should be selected from the DragOver event
+        Dim targetNode As TreeNode = selectedTreeview.SelectedNode
+
+        If selectedTreeview.Nodes.Find(dropNode.Name, True).Count = 0 Then _
+            selectedTreeview.Nodes.Add(dropNode.Name, dropNode.Text)
+
+    End Sub
+
+#End Region
+
+
+
+End Class
