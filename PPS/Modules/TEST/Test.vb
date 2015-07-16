db3f9d7601a3f188afef209bcfa564385198c59a
Nathanael <nathanael.landais@pps-bi.com>
2015-07-16 09:59:54 +0200

    Test UI

--- PPS/Modules/TEST/Test.vb
+++ PPS/Modules/TEST/Test.vb
@@ -2,13 +2,24 @@
 
     Public Sub New()
         InitializeComponent()
-        NetworkManager.GetInstance().SetCallback(ServerMessage.SMSG_TEST_ANSWER, AddressOf CMSG_TEST)
+        NetworkManager.GetInstance().SetCallback(ServerMessage.SMSG_TEST_ANSWER, AddressOf SMSG_TEST_ANSWER)
+    End Sub
+
+    Private Sub SMSG_TEST_ANSWER(packet As ByteBuffer)
+        MsgBox(packet.ReadString())
     End Sub
 
     Private Sub CMSG_TEST()
         Dim packet As New ByteBuffer(CType(ClientMessage.CMSG_TEST, UShort))
+        Dim test_array() As Int32 = {2, 6, 5, 3}
 
-        packet.WriteInt32(10)
+        packet.WriteInt32(test_array.Length)
+        For Each value In test_array
+            packet.WriteInt32(value)
+        Next
+        packet.Release()
+
+        NetworkManager.GetInstance().Send(packet)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
