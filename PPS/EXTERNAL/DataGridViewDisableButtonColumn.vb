469a26f96d09947717575a695dddc87953d192ee
jjmonn <monnereau.julien@gmail.com>
2015-09-01 13:18:08 +0200

    Unecessary files deletion

--- PPS/EXTERNAL/DataGridViewDisableButtonColumn.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Windows.Forms
-
-Public Class DataGridViewDisableButtonColumn
-    Inherits DataGridViewButtonColumn
-
-    Public Sub New()
-        Me.CellTemplate = New DataGridViewDisableButtonCell()
-    End Sub
-End Class
