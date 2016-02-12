fb652b8281d470b8e91bfe0712435bb2c82449a4
Benjamin COUILLAUD <benjamin.couillaud@epitech.eu>
2016-02-12 12:25:23 +0100

    CuiDgvConf : Allows to send config to the dataGridView, in the CUI

---
+++ Excel FinancialBI/Excel FinancialBI/MVC/Model/CRUD/CuiDgvConf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBI.MVC.Model.CRUD
+{
+  class CuiDgvConf
+  {
+    public object Type { get; set; }
+    public UInt32 FilterId { get; set; }
+    public AxisType AxisTypeId { get; set; }
+    public UInt32 StartPeriod { get; set; }
+    public UInt32 NbPeriods { get; set; }
+    public TimeConfig PeriodRange { get; set; }
+  }
+}
