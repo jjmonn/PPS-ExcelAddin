bf81c3c91635394ac94a707fdb749e3972256722
Nathanael <nathanael.landais@pps-bi.com>
2016-03-14 15:51:01 +0100

    Reorder CUI Visualization

--- Excel FinancialBI/Excel FinancialBI/MVC/View/Controlling UI/CUIVisualization.cs
+++ Excel FinancialBI/Excel FinancialBI/MVC/View/Controlling UI/CUIVisualization.cs
@@ -17,15 +17,9 @@ namespace FBI.MVC.View
   using Utils;
   using Model.CRUD;
 
-  using GraphSortedDic = SafeDictionary<UInt32, SafeDictionary<Int32, SafeDictionary<Tuple<bool, Model.CRUD.AxisType, UInt32>, double>>>;
-  using GraphUnSortedDic = SafeDictionary<UInt32, SafeDictionary<Int32, List<double>>>;
-
   public partial class CUIVisualization : Form, IView
   {
     CUIVisualizationController m_controller;
-    Random m_rand = new Random();
-    SafeDictionary<ResultKey, double> m_values;
-    SafeDictionary<ResultKey, double> m_accountValues;
 
     public CUIVisualization()
     {
@@ -56,46 +50,6 @@ namespace FBI.MVC.View
       panel2.ContextMenuStrip = m_panelRightClick;
       m_horizontalSplitBT.Click += OnSplitHorizontalClick;
       m_splitVerticalBT.Click += OnSplitVerticalClick;
-    }
-
-    void LoadData(SafeDictionary<ResultKey, double> p_values)
-    {
-      m_values = p_values;
-      SelectAccount(1);
-    }
-
-    void SelectAccount(UInt32 p_accountId)
-    {
-      m_accountValues = new SafeDictionary<ResultKey, double>();
-
-      foreach (KeyValuePair<ResultKey, double> l_pair in m_values)
-        if (l_pair.Key.AccountId == p_accountId)
-          m_accountValues[l_pair.Key] = l_pair.Value;
-    }
-
-    GraphSortedDic BuildSortedValues(bool p_isAxis, AxisType p_axis, UInt32 p_id = 0)
-    {
-      GraphSortedDic l_sortedValues = new GraphSortedDic();
-
-      foreach (KeyValuePair<ResultKey, double> l_pair in m_accountValues)
-        if (l_pair.Key.IsSort(p_isAxis, p_axis, p_id))
-        {
-          Tuple<bool, AxisType, UInt32> l_sort = l_pair.Key.LastSort;
-
-          if (l_sort != null)
-            l_sortedValues[l_pair.Key.VersionId][l_pair.Key.Period][l_sort] = l_pair.Value;
-        }
-      return (l_sortedValues);
-    }
-
-    GraphUnSortedDic BuildUnsortedValues()
-    {
-      GraphUnSortedDic l_values = new GraphUnSortedDic();
-
-      foreach (KeyValuePair<ResultKey, double> l_pair in m_accountValues)
-        if (l_pair.Key.SortHash == "")
-          l_values[l_pair.Key.VersionId][l_pair.Key.Period].Add(l_pair.Value);
-      return (l_values);
     }
 
     Chart CreateChart()
