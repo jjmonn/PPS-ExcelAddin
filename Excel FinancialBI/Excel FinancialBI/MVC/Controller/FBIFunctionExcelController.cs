2fa8126c1f2316cfda768832fd9a6d7687a6bc93
Nathanael <nathanael.landais@pps-bi.com>
2016-03-21 18:07:32 +0100

    Add in excel FBI Function validity check

--- Excel FinancialBI/Excel FinancialBI/MVC/Controller/FBIFunctionExcelController.cs
+++ Excel FinancialBI/Excel FinancialBI/MVC/Controller/FBIFunctionExcelController.cs
@@ -9,12 +9,13 @@ using Microsoft.Office.Interop.Excel;
 namespace FBI.MVC.Controller
 {
   using View;
+  using Model.CRUD;
 
   public class FBIFunctionExcelController : AFBIFunctionController
   {
     public override IView View { get { return (null); } }
 
-    string GetValue(object p_param)
+    static string GetValue(object p_param)
     {
       if (p_param.GetType() == typeof(string))
         return (p_param as string);
@@ -31,7 +32,7 @@ namespace FBI.MVC.Controller
       return ("");
     }
 
-    List<string> GetValueList(object p_param)
+    static List<string> GetValueList(object p_param)
     {
       List<string> l_list = new List<string>();
       if (p_param.GetType().IsArray == false)
@@ -47,9 +48,36 @@ namespace FBI.MVC.Controller
       return (l_list);
     }
 
+    bool IsValidFunction(FBIFunction p_function)
+    {
+      return (
+              IsValidEntity(p_function.Entity) &&
+              IsValidAccount(p_function.Account) &&
+              IsValidCurrency(p_function.Currency) &&
+              IsValidVersion(p_function.Version) &&
+              IsValidAxisElemList(AxisType.Client, p_function.AxisElems[AxisType.Client]) &&
+              IsValidAxisElemList(AxisType.Product, p_function.AxisElems[AxisType.Product]) &&
+              IsValidAxisElemList(AxisType.Adjustment, p_function.AxisElems[AxisType.Adjustment]) &&
+              IsValidFilterValueList(p_function.Filters));
+    }
+
     public object FBI(object p_entity, object p_account, object p_period, object p_currency, object p_version,
           object p_clientsFilters, object p_productsFilters, object p_adjustmentsFilters, object p_categoriesFilters)
     {
+      FBIFunction l_function = new FBIFunction();
+
+      l_function.Entity = GetValue(p_entity);
+      l_function.Account = GetValue(p_account);
+      l_function.Period = GetValue(p_period);
+      l_function.Currency = GetValue(p_currency);
+      l_function.Version = GetValue(p_version);
+      l_function.AxisElems[AxisType.Client] = GetValueList(p_clientsFilters);
+      l_function.AxisElems[AxisType.Product] = GetValueList(p_productsFilters);
+      l_function.AxisElems[AxisType.Adjustment] = GetValueList(p_adjustmentsFilters);
+      l_function.Filters = GetValueList(p_categoriesFilters);
+
+      if (IsValidFunction(l_function) == false)
+        return (Error);
       return (null);
     }
   }
---
+++ Excel FinancialBI/Excel FinancialBI/MVC/Model/CRUD/FBIFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBI.MVC.Model.CRUD
+{
+  using Utils;
+
+  class FBIFunction
+  {
+    public string Entity { get; set; }
+    public string Account { get; set; }
+    public string Period { get; set; }
+    public string Currency { get; set; }
+    public string Version { get; set; }
+    public SafeDictionary<AxisType, List<string>> AxisElems { get; set; }
+    public List<string> Filters { get; set; }
+
+    public FBIFunction()
+    {
+      AxisElems = new SafeDictionary<AxisType, List<string>>();
+      Filters = new List<string>();
+    }
+  }
+}
