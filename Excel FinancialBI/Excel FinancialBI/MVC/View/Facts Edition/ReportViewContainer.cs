d90fb6d67dc5c4778c2367082494aae761b0e75d
jjmonn <monnereau.julien@gmail.com>
2016-06-28 22:14:32 +0200

    report uplad uplaod views design fixes

--- Excel FinancialBI/Excel FinancialBI/MVC/View/Facts Edition/ReportViewContainer.cs
+++ Excel FinancialBI/Excel FinancialBI/MVC/View/Facts Edition/ReportViewContainer.cs
@@ -8,19 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace FBI.MVC.View
 {
+  using Utils;
+
   public partial class ReportViewContainer : Form
   {
     public ReportViewContainer()
     {
       InitializeComponent();
+      this.Text = Local.GetValue("upload.title_report_view");
     }
 
     private void ReportViewContainer_FormClosing(object sender, FormClosingEventArgs e)
     {
       e.Cancel = true;
       Hide();
+    }
+
+    private void ReportViewContainer_Load(object sender, EventArgs e)
+    {
+
     }
   }
 }
